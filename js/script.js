3bc8e1d949d53d526c4dd5e7cd140ebbffe7e656
rapus <ruslanraiter@gmail.com>
2019-03-26 22:24:25 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    $('.slider .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        dots: false,
+        navText: [],
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+                nav: false,
+            },
+            480: {
+                items: 1,
+            }
+        }
+    });
+
+    $('.carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots: false,
+        navText: [],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            480: {
+                items: 2,
+            },
+            680:{
+                items: 3,
+            },
+            1024:{
+                items: 4,
+            },
+
+        }
+    });
+});
